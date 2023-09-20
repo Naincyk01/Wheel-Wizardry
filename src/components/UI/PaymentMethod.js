ed9d3d7a33a1e31e9ca0a2d366e7e302496e981f
Naincy <naincyk15042004@gmail.com>
2023-09-20 22:48:54 +0530

    Modify the CSS of PaymentForm

--- src/components/UI/PaymentMethod.js
+++ src/components/UI/PaymentMethod.js
@@ -8,34 +8,34 @@ const PaymentMethod = () => {
   return (
     <>
       <div className="payment">
-        <label htmlFor="" className="d-flex align-items-center gap-2">
+        <label htmlFor="" className="flex items-center gap-2 font-semibold text-[#000d6b]">
           <input type="radio" /> Direct Bank Transfer
         </label>
       </div>
 
       <div className="payment mt-3">
-        <label htmlFor="" className="d-flex align-items-center gap-2">
+        <label htmlFor="" className="flex items-center gap-2 font-semibold text-[#000d6b]">
           <input type="radio" /> Cheque Payment
         </label>
       </div>
 
-      <div className="payment mt-3 d-flex align-items-center justify-content-between">
-        <label htmlFor="" className="d-flex align-items-center gap-2">
+      <div className="payment mt-3 flex items-center justify-between">
+        <label htmlFor="" className="flex items-center gap-2 font-semibold text-[#000d6b]">
           <input type="radio" /> Master Card
         </label>
 
         <img src={masterCard} alt="" />
       </div>
 
-      <div className="payment mt-3 d-flex align-items-center justify-content-between">
-        <label htmlFor="" className="d-flex align-items-center gap-2">
+      <div className="payment mt-3 flex items-center justify-between">
+        <label htmlFor="" className="flex items-center gap-2 font-semibold text-[#000d6b]">
           <input type="radio" /> Paypal
         </label>
 
         <img src={paypal} alt="" />
       </div>
       <div className="payment text-end mt-5">
-        <button>Reserve Now</button>
+        <button className="py-[8px] px-[15px] rounded bg-[#000d6b] text-[#fff] border-none outline-none">Reserve Now</button>
       </div>
     </>
   );
